dd1bac38eb07f3ed2590afaebdc58ce378cf96dd
Davide Lo Re <boyska@gmail.com>
2009-04-05 12:30:14 +0200

    separated PGencode.htm in example.php + form.htm
    Now it's easily embeddable in any web page, with just an include("form.hm")
    Added a TODO

---
+++ example.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head>
+
+
+ <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+ <meta name="resource-type" content="document">
+ <meta name="author" content="Herbert Hanewinkel">
+ <meta name="keywords" content="OpenPGP, GnuPG, PGP, RSA, AES, Rijndael, public key encryption">
+ <meta name="keywords" content="PKI, Javascript, RFC 2440">
+ <meta name="keywords" content="mail encryption">
+<title>OpenPGP Message Encryption in JavaScript Demo</title>
+
+<script language="Javascript" src="PGencode_files/rsa.js" type="text/javascript"></script>
+<script language="Javascript" src="PGencode_files/aes-enc.js" type="text/javascript"></script>
+<script language="Javascript" src="PGencode_files/base64.js" type="text/javascript"></script>
+<script language="Javascript" src="PGencode_files/mouse.js" type="text/javascript"></script>
+<script language="Javascript" src="PGencode_files/PGencode.js" type="text/javascript"></script>
+
+</head><body onload="load()" bgcolor="#d0d0d0">
+
+<h2 align="center">OpenPGP Message Encryption in JavaScript Demo</h2>
+<center><font size="-1">© 2005 Herbert Hanewinkel</font></center>
+<hr>
+<center>
+[<a href="http://www.hanewin.net/encrypt/PGpubkey.htm">OpenPGP Key extraction</a>][OpenPGP Encryption]
+</center>
+
+<?php include("form.htm") ?>
+
+<hr align="center" width="99%">
+<table width="100%"><tbody><tr>
+<td><font size="-1"><i>www.haneWIN.net</i></font></td>
+<td align="right"><font size="-1"><i>
+<a href="http://www.hanewin.net/" target="_top">Home</a>
+</i></font></td>
+</tr></tbody></table>
+</body></html>
